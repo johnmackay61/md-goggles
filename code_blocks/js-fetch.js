e89fa4b0947b706afeb6761fc821703778cd416d
John Mackay <johnmackay61@users.noreply.github.com>
2025-03-16 09:18:40 +0700

    Delete code_blocks directory
    No longer needed

--- code_blocks/js-fetch.js
+++
@@ -1,6 +0,0 @@
-fetch( 'https://example.com/file.md' )
-  .then( response => response.text() )
-  .then( (result) => {
-    var content = marked.parse(result);
-    document.querySelector( '#my-container' ).innerHTML = content;
-  });
--- code_blocks/parsing-snippet.js
+++
@@ -1,9 +0,0 @@
-const markdownParser = (text) => {
-	const toHTML = text
-		.replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
-		.replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
-		.replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
-		.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
-		.replace(/\*(.*)\*/gim, '<i>$1</i>'); // italic text
-	return toHTML.trim(); // using trim method to remove whitespace
-}
